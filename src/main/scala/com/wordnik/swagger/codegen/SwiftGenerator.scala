4365f0a8fd9b5f08ef8f57a6846f1c91eccc43b1
Nate Stedman <natesm@gmail.com>
2014-12-08 17:12:06 -0500

    Expansion of swift work

--- src/main/scala/com/wordnik/swagger/codegen/SwiftGenerator.scala
+++ src/main/scala/com/wordnik/swagger/codegen/SwiftGenerator.scala
@@ -7,12 +7,223 @@ object SwiftGenerator extends SwiftGenerator {
 }
 
 class SwiftGenerator extends BasicGenerator {
-    // location of templates
-    override def templateDir = "swift"
-    
-    // template used for models
-    modelTemplateFiles += "model.mustache" -> ".swift"
+  override def defaultIncludes = Set(
+    "bool",
+    "int",
+    "NSString",
+    "NSObject", 
+    "NSArray",
+    "NSNumber")
 
-    // template used for apis
-    apiTemplateFiles += "api.mustache" -> ".swift"
+  override def languageSpecificPrimitives = Set(
+    "NSNumber",
+    "NSString",
+    "NSObject",
+    "bool")
+
+  override def reservedWords = Set("void", "char", "short", "int", "void", "char", "short", "int", "long", "float", "double", "signed", "unsigned", "id", "const", "volatile", "in", "out", "inout", "bycopy", "byref", "oneway", "self", "super")
+
+  def foundationClasses = Set(
+    "NSNumber",
+    "NSObject",
+    "NSString")
+  
+  override def typeMapping = Map(
+    "enum" -> "NSString",
+    "date" -> "SWGDate",
+    "Date" -> "SWGDate",
+    "boolean" -> "NSNumber",
+    "string" -> "NSString",
+    "integer" -> "NSNumber",
+    "int" -> "NSNumber",
+    "float" -> "NSNumber",
+    "long" -> "NSNumber",
+    "double" -> "NSNumber",
+    "Array" -> "NSArray",
+    "array" -> "NSArray",
+    "List" -> "NSArray",
+    "object" -> "NSObject")
+
+  override def importMapping = Map(
+    "Date" -> "SWGDate")
+
+  override def toModelFilename(name: String) = "SWG" + name
+
+  // naming for the models
+  override def toModelName(name: String) = {
+    (typeMapping.keys ++ 
+      foundationClasses ++ 
+      importMapping.values ++ 
+      defaultIncludes ++ 
+      languageSpecificPrimitives
+    ).toSet.contains(name) match {
+      case true => name(0).toUpper + name.substring(1)
+      case _ => {
+        "SWG" + name(0).toUpper + name.substring(1)
+      }
+    }
+  }
+
+  // objective c doesn't like variables starting with "new"
+  override def toVarName(name: String): String = {
+    val paramName = name.replaceAll("[^a-zA-Z0-9_]","")
+    if(paramName.startsWith("new") || reservedWords.contains(paramName)) {
+      escapeReservedWord(paramName)
+    }
+    else paramName
+  }
+
+  // naming for the apis
+  override def toApiName(name: String) = "SWG" + name(0).toUpper + name.substring(1) + "Api"
+
+  // location of templates
+  override def templateDir = "swift"
+
+  // template used for models
+  modelTemplateFiles += "model.mustache" -> ".swift"
+
+  // template used for apis
+  apiTemplateFiles += "api.mustache" -> ".swift"
+
+  // package for models
+  override def invokerPackage: Option[String] = None
+
+  // package for models
+  override def modelPackage: Option[String] = None
+
+  // package for api classes
+  override def apiPackage: Option[String] = None
+
+  // response classes
+  override def processResponseClass(responseClass: String): Option[String] = {
+    typeMapping.contains(responseClass) match {
+      case true => Some(typeMapping(responseClass))
+      case false => {
+        responseClass match {
+          case "void" => None
+          case e: String => {
+            if(responseClass.toLowerCase.startsWith("array") || responseClass.toLowerCase.startsWith("list"))
+              Some("NSArray")
+            else
+              Some(toModelName(responseClass))
+          }
+        }
+      }
+    }
+  }
+
+  override def processApiMap(m: Map[String, AnyRef]): Map[String, AnyRef] = {
+    val mutable = scala.collection.mutable.Map() ++ m
+    mutable += "newline" -> "\n"
+
+    mutable.map(k => {
+      k._1 match {
+        case e: String if (e == "allParams") => {
+          val sp = (mutable(e)).asInstanceOf[List[_]]
+          sp.size match {
+            case i: Int if(i > 0) => mutable += "hasParams" -> "true"
+            case _ =>
+          }
+        }
+        case _ =>
+      }
+    })
+    mutable.toMap
+  }
+
+  override def processResponseDeclaration(responseClass: String): Option[String] = {
+    processResponseClass(responseClass) match {
+      case Some("void") => Some("void")
+      case Some(e) => Some(e + "*")
+      case _ => Some(responseClass)
+    }
+  }
+
+  override def toDeclaredType(dt: String): String = {
+    val declaredType = dt.indexOf("[") match {
+      case -1 => dt
+      case n: Int => "NSArray"
+    }
+    val t = typeMapping.getOrElse(declaredType, declaredType)
+
+    (languageSpecificPrimitives.contains(t) && !foundationClasses.contains(t)) match {
+      case true => toModelName(t)
+      case _ => toModelName(t) + "*" // needs pointer
+    }
+  }
+
+  override def toDeclaration(obj: ModelProperty) = {
+    var declaredType = toDeclaredType(obj.`type`)
+    declaredType.toLowerCase match {
+      case "list" => {
+        declaredType = "array"
+      }
+      case e: String => e
+    }
+
+    val defaultValue = toDefaultValue(declaredType, obj)
+    declaredType match {
+      case "array" => {
+        val inner = {
+          obj.items match {
+            case Some(items) => {
+              if(items.ref != null) 
+                items.ref
+              else
+                items.`type`
+            }
+            case _ => {
+              println("failed on " + obj)
+              throw new Exception("no inner type defined")
+            }
+          }
+        }
+        "NSArray"
+      }
+      case "set" => {
+        val inner = {
+          obj.items match {
+            case Some(items) => items.ref.getOrElse(items.`type`)
+            case _ => {
+              println("failed on " + obj)
+              throw new Exception("no inner type defined")
+            }
+          }
+        }
+        "NSArray"
+      }
+      case _ =>
+    }
+    (declaredType, defaultValue)
+  }
+
+  override def escapeReservedWord(word: String) = "_" + word
+
+  override def toDefaultValue(properCase: String, obj: ModelProperty) = {
+    properCase match {
+      case "boolean" => "false"
+      case "int" => "0"
+      case "long" => "0L"
+      case "float" => "0.0f"
+      case "double" => "0.0"
+      case "List" => {
+        val inner = {
+          obj.items match {
+            case Some(items) => {
+              if(items.ref != null) 
+                items.ref
+              else
+                items.`type`
+            }
+            case _ => {
+              println("failed on " + properCase + ", " + obj)
+              throw new Exception("no inner type defined")
+            }
+          }
+        }
+        "new ArrayList<" + inner + ">" + "()"
+      }
+      case _ => "null"
+    }
+  }
 }

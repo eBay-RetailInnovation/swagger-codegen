c74f9f46306cd36f4c27dc56bf452cb96c34703d
Nate Stedman <natesm@gmail.com>
2014-12-08 19:07:05 -0500

    Modifications to API names

--- src/main/scala/com/wordnik/swagger/codegen/SwiftGenerator.scala
+++ src/main/scala/com/wordnik/swagger/codegen/SwiftGenerator.scala
@@ -62,15 +62,20 @@ class SwiftGenerator extends BasicGenerator {
 
   // objective c doesn't like variables starting with "new"
   override def toVarName(name: String): String = {
-    val paramName = name.replaceAll("[^a-zA-Z0-9_]","") match {
+    val paramName = name.replaceAll("[^a-zA-Z0-9_]","")
+    
+    val identifierFiltered = paramName match {
         case "id" => "identifier"
         case other => other
     }
     
-    if(paramName.startsWith("new") || reservedWords.contains(paramName)) {
-      escapeReservedWord(paramName)
+    val identifierExpanded = identifierFiltered.replaceAll("Id", "Identifier")
+    val acronymsCapitalized = identifierExpanded.replaceAll("Url", "URL")
+    
+    if(reservedWords.contains(acronymsCapitalized)) {
+      escapeReservedWord(acronymsCapitalized)
     }
-    else paramName
+    else acronymsCapitalized
   }
 
   // naming for the apis
